e751350f139afc87e60928608b5a0890b5cc93d1
mohamad akar <MoeAkar123456@gmail.com>
2025-02-19 19:50:44 +0200

    new repo

---
+++ ClientSide/src/App.jsx
@@ -0,0 +1,12 @@
+import {BrowserRouter,Routes,Route, Navigate} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+
+function App() {
+  
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  )
+}
+
+export default App
