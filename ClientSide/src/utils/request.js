e581a999621ca13d139296faa60cb95d2ffcbfcc
Mhmd akar <moeakar123456@gmail.com>
2025-06-28 00:10:46 +0300

    last one

--- ClientSide/src/utils/request.js
+++ ClientSide/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const request=axios.create({
-    baseURL:"http://localhost:4500"
+    baseURL:["http://localhost:4500", "https://intelligram.onrender.com"].find(url => url.startsWith('https')) || "http://localhost:4500",
 })
 
 export default request;
